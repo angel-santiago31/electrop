c278c087d25516a5e7c649f1cd7bcaee311d2b7e
Bryan Hernandez <bryan.hernandez5@upr.edu>
2017-04-05 18:35:52 -0400

    Changes to sticker page

--- frontend/views/site/stickers.php
+++ frontend/views/site/stickers.php
@@ -18,39 +18,43 @@ $this->title = 'Stickers';
 
 <h1> Stickers </h1>
 
-<div class="container" style="background-color: white; position: relative;">
-    <div class="list-group" style="float: left; position: relative;">
-            <button type="button" class="list-group-item">Categories</button>
-            <button type="button" class="list-group-item">Sub Categories</button>
-            <label type="label" class="list-group-item">Sort</button>
-            <button type="button" class="list-group-item">Price</button>
-            <button type="button" class="list-group-item">Date Added</button>
-    </div>
-</div>
+<div class="container" style="background-color: white;">
   <div class="container" style="background-color: lightgray; position: relative;">
-        <div class="row" style="float: right;  position: relative;">
-            <div class="col-sm-12">
+        <div class="row" style="float: center;  position: relative;">
+            
+            <div class="col-sm-12" >
+            <div class="col-sm-2" style="float: left; position: relative;">
+                <div class="list-group" style="float: left; position: relative;">
+                    <button type="button" class="list-group-item">Categories</button>
+                    <button type="button" class="list-group-item">Sub Categories</button>
+                    <label type="label" class="list-group-item">Sort</button>
+                    <button type="button" class="list-group-item">Price</button>
+                    <button type="button" class="list-group-item">Date Added</button>
+                </div>
+            </div>
             <?php 
-                $stickersController = Item::find()->limit(7)->all();
-                foreach($stickersController as $sticker){
-            ?>
+    $stickersController = Item::find()->limit(7)->all();
+    foreach($stickersController as $sticker){
+?>
+                        <div class="col-sm-2" style="float: left; position: relative; text-align: center;">
+                            <img src='<?= Url::to($sticker->picture); ?>' height="150" width="150" />
                             <div class="row">
-                                        <div class="col-sm-4" style="text-align: center;">
-                                            <img src='<?= Url::to($sticker->picture); ?>' height="150" width="150" />
-                                            <div class="row">
-                                                <div class="col-sm-12" style="text-align: center;">
-                                                    <?php echo $sticker->name  ?>
-                                                    <br>
-                                                    <?php echo '$' . $sticker->gross_price ?>
-                                                    <br>
-                                                    <?= Html::a('label', ['/site/stickers'], ['class'=>'btn btn-primary']) ?>
-                                                </div>
-                                            </div>
-                                        </div>
-                                <?php  } ?>
+                                <div class="col-sm-12" style="text-align: center;">
+                                    <?php echo $sticker->name  ?>
+                                    <br>
+                                    <?php echo '$' . $sticker->gross_price ?>
+                                    <br>
+                                    <?= Html::a('View Details', ['/site/stickers'], ['class'=>'btn btn-primary']) ?>
+                                    <br>
+                                    <br>
+                                    <br>
+                                </div>
                             </div>
+                        </div>
+                <?php  } ?>
             </div>
         </div>            
+</div>
 </div>
 
 

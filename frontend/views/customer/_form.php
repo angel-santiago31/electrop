7124098fb4bc06b49254a71e2cfe41f83bac59c0
Angel Santiago <angel.santiago31@upr.edu>
2017-04-11 16:50:03 -0400

    Frontend Customer Update

--- frontend/views/customer/_form.php
+++ frontend/views/customer/_form.php
@@ -1,53 +1,23 @@
 <?php
 
 use yii\helpers\Html;
-use yii\helpers\Url;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Customer */
 /* @var $form yii\widgets\ActiveForm */
-
-
-
 ?>
 
-<h1><?= $model->first_name . ' ' . $model->fathers_last_name; ?></h1>
+<div class="customer-form">
 
-<div class="container" align="center"> 
-                <div class="row">
-            <div class="col-sm-12">
-                <?php $form = ActiveForm::begin(['id' => 'customer-account-form']); ?>
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <?= $form->field($model, 'email')->textInput(['value' => $model->email]) ?>
-                        </div>
-                        <div class="col-sm-4">
-                            <?= $form->field($model, 'first_name')->textInput() ?>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <?= $form->field($model, 'middle_name')->label('Middle Name (Optional)') ?>
-                        </div>
-                        <div class="col-sm-4">
-                            <?= $form->field($model, 'fathers_last_name')->textInput() ?>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <?= $form->field($model, 'mothers_last_name')->textInput()?>
-                        </div>
-                        <div class="col-sm-4">
-                            <?= $form->field($model, 'date_of_birth')->textInput() ?>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <?=
-                            Html::submitButton('<i class="glyphicon glyphicon-pencil"></i> Change', [ 'value' => Url::to(['/customer/update-info']), 'class' => 'btn btn-danger redCss', 'name' => 'update-customer-info-button']);
-                        ?>
-                    </div>
-            <?php ActiveForm::end(); ?>
+    <?php $form = ActiveForm::begin(); ?>
 
-        </div>
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
--- frontend/views/customer/update.php
+++ frontend/views/customer/update.php
@@ -3,14 +3,15 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model common\models\model */
+/* @var $model common\models\Customer */
 
-$this->title = $model->first_name . ' ' . $model->fathers_last_name;
-$this->params['breadcrumbs'][] = ['label' => 'models', 'url' => ['index']];
-//$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';  
+$this->title = 'Update Customer: ' . $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Customers', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="customer-update">
+
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [

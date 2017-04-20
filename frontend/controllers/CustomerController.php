f4886d0e7bb935ee78b7ac0f5274540004549820
Angel Santiago <angel.santiago31@upr.edu>
2017-04-19 22:42:31 -0400

    Oops

--- frontend/controllers/CustomerController.php
+++ frontend/controllers/CustomerController.php
@@ -99,6 +99,25 @@ class CustomerController extends Controller
     }
 
     /**
+     * Updates an existing Customer Phone Number model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdatePhone($id)
+    {
+        $model = $this->findPhone($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['account', 'id' => $model->customer_id]);
+        } else {
+            return $this->renderAjax('_updatePhone', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
      * Deletes an existing Customer model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      * @param integer $id

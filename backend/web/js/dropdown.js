d847b26f5ea7e9eb6d69c4a9b39d86d2dc2d1f6a
Bryan Hernandez <bryan.hernandez5@upr.edu>
2017-04-24 12:06:42 -0400

    Commit pq Angel me dijo

--- backend/web/js/dropdown.js
+++ backend/web/js/dropdown.js
@@ -2,7 +2,7 @@ document.getElementById("itemIdField").hidden = true;
 $("#refers_to").change(function(){
     var ddValue = $('#refers_to').val();
 
-    if(ddValue == 4) {
+    if(ddValue == 4) { 
         document.getElementById("itemIdField").hidden = false;
     } else {
         document.getElementById("itemIdField").hidden = true;     

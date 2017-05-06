7039dc3e5a978bf7028387fde3e29dfded5a1ef4
Bryan Hernandez <bryan.hernandez5@upr.edu>
2017-05-06 10:56:57 -0400

    Order Status Disabled button Bug Fixed

--- backend/web/js/order-status.js
+++ backend/web/js/order-status.js
@@ -1,7 +1,9 @@
 $(function() {
    $('#updateOrderStatus').click(function() {
-       $('#modalS').modal('show')
+       if($('#updateOrderStatus').prop('enabled')){
+            $('#modalS').modal('show')
            .find('#modalContent')
            .load($(this).attr('value'));
+       }
    });
 });
